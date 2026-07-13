1925a189160b4b12c7e55d4be6059d7df60807db
williamaris <william.aris@usherbrooke.ca>
2026-07-13 19:57:37 -0400

    Evaluate script done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# voxbench
+# VoxBench
+
+Get a copy of the dataset:
+
+```bash
+python -m voxbench.get_dataset --dataset <dataset_name> --output <output_dir>
+```
+
+Benchmark processed audio:
+
+```bash
+python -m voxbench.evaluate --dataset <dataset_name> --input <processed_audio_dir> --output <results_dir>
+```
+
+Compile results into a table:
+
+```bash
+python -m voxbench.compile_results --input <list_file> --format <markdown || latex || csv>
+```
+
+optional 
+- ```--precision <n_of_decimals>```
+- ```--no_sd```
--- voxbench/evaluate.py
+++ voxbench/evaluate.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import json
 import shutil
 import argparse
 import numpy as np
@@ -33,11 +34,14 @@ parser.add_argument(
     '-o',
     '--output',
     type = str,
-    required = True,
+    required = False,
     help = "Output directory where to save the results."
 )
 
 args = parser.parse_args()
+
+if args.output is None:
+    args.output = os.path.join(args.input, "..", "voxbench", args.dataset)
 
 # Instantiate dataset
 args.dataset = args.dataset.strip()
@@ -56,15 +60,15 @@ for f, _, _ in dataset:
         raise FileNotFoundError(f"{filepath} not found.")
 
 # Preparing results dict
-results = {}
+results = {"dataset": args.dataset}
 metrics = ["pesq", "stoi", "si-snr", "si-sdr", "csig", "cbak", "covl"]
 
 for metric in metrics:
     results[metric] = {"noisy": [], "processed": []}
 
 def log_result(metric, noisy_score, processed_score):
-    results[metric]['noisy'].append(noisy_score)
-    results[metric]['processed'].append(processed_score)
+    results[metric]['noisy'].append(noisy_score.tolist())
+    results[metric]['processed'].append(processed_score.tolist())
 
 # Main processing loop
 for filename, xs_data, ss_data in tqdm(dataset, desc = "Benchmarking"):
@@ -99,13 +103,44 @@ for filename, xs_data, ss_data in tqdm(dataset, desc = "Benchmarking"):
 
     log_result('si-sdr', noisy_si_sdr, processed_si_sdr)
 
-    noisy_csig, noisy_cbak, noisy_covl = m.compute_composite(xs, ss, fs)
-    processed_csig, processed_cbak, processed_covl = m.compute_composite(ys, ss, fs)
+    #noisy_csig, noisy_cbak, noisy_covl = m.compute_composite(xs, ss, fs)
+    #processed_csig, processed_cbak, processed_covl = m.compute_composite(ys, ss, fs)
 
-    log_result('csig', noisy_csig, processed_csig)
-    log_result('cbak', noisy_cbak, processed_cbak)
-    log_result('covl', noisy_covl, processed_covl)
+    #log_result('csig', noisy_csig, processed_csig)
+    #log_result('cbak', noisy_cbak, processed_cbak)
+    #log_result('covl', noisy_covl, processed_covl)
+
+# Compiling final results
+summary = "="*61
+summary += "\n" +  f" BENCHMARK RESULTS FOR DATASET: {args.dataset}"
+summary += "\n" + "="*61
+summary += "\n" + f"| {'Metric':<10} | {'Noisy (Mean ± SD)':<20} | {'Processed (Mean ± SD)':<21} |"
+summary += "\n" + f"|{'-'*12}|{'-'*22}|{'-'*23}|"
 
 for metric in metrics:
-    print(f"{metric}: {np.mean(results[metric]['noisy'])}")
+    noisy_vals = np.array(results[metric]['noisy'])
+    proc_vals = np.array(results[metric]['processed'])
 
+    n_mean, n_std = np.mean(noisy_vals), np.std(noisy_vals)
+    p_mean, p_std = np.mean(proc_vals), np.std(proc_vals)
+
+    results[metric]["noisy_mean"] = float(n_mean)
+    results[metric]["noisy_std"] = float(n_std)
+    results[metric]["processed_mean"] = float(p_mean)
+    results[metric]["processed_std"] = float(p_std)
+
+    summary += "\n" + f"| {metric.upper():<10} | {n_mean:>6.4f} ± {n_std:<10.4f} | {p_mean:>9.4f} ± {p_std:<11.4f} |"
+
+summary += "\n" + "="*61
+
+print("\n" + summary)
+
+# Saving results
+if not os.path.exists(args.output):
+    os.makedirs(args.output)
+
+with open(os.path.join(args.output, 'voxbench_results.json'), 'w') as f:
+    json.dump(results, f, indent = 4)
+
+with open(os.path.join(args.output, 'voxbench_summary.txt'), 'w') as f:
+    f.write(summary)
